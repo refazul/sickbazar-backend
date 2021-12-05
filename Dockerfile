75e0fd4072317623f1ecaf65c384ea99299e5a97
refazul <refazul.refat@yahoo.com>
2021-12-05 17:58:50 +0600

    init

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:17
+WORKDIR /app
+COPY package.json /app/package.json
+RUN npm install
+
+COPY src/ /app/src/
+EXPOSE 4000
+CMD [ "node", "src/index.js" ]
+
+# docker build . -t refazul/sickbazar-backend
+# docker run -dit -p 4000:4000 --name sickbazar-backend --env-file .env refazul/sickbazar-backend
+# docker image prune
---
+++ src/utils.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+module.exports.paginateResults = ({
+    after: cursor,
+    pageSize = 20,
+    results,
+    // can pass in a function to calculate an item's cursor
+    getCursor = () => null,
+}) => {
+    if (pageSize < 1) return [];
+
+    if (!cursor) return results.slice(0, pageSize);
+    const cursorIndex = results.findIndex(item => {
+        // if an item has a `cursor` on it, use that, otherwise try to generate one
+        let itemCursor = item.cursor ? item.cursor : getCursor(item);
+
+        // if there's still not a cursor, return false by default
+        return itemCursor ? cursor === itemCursor : false;
+    });
+
+    return cursorIndex >= 0
+        ? cursorIndex === results.length - 1 // don't let us overflow
+            ? []
+            : results.slice(
+                cursorIndex + 1,
+                Math.min(results.length, cursorIndex + 1 + pageSize),
+            )
+        : results.slice(0, pageSize);
+};
+
+module.exports.createStore = () => {
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://' + process.env.db_user_mongodb + ':' + process.env.db_pass_mongodb + '@' + process.env.db_host + ':' + process.env.db_port_mongodb + '/' + process.env.db_name_mongodb + '?authSource=admin&readPreference=primary&directConnection=true&ssl=false', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    UserSchema = mongoose.model('User', {
+        email: String,
+        token: String
+    });
+    TripSchema = mongoose.model('Trip', {
+        launchId: Number,
+        userId: String
+    })
+
+    const User = {
+        findOrCreate: async ({ email }) => {
+            var user = await UserSchema.findOne({ email }).exec();
+            if (!user) {
+                var newUser = new UserSchema({ email });
+                user = await newUser.save();
+            }
+            return [user];
+        },
+    }
+    const Trip = {
+        findOrCreate: async ({ userId, launchId }) => {
+            var trip = await TripSchema.findOne({ userId, launchId }).exec();
+            if (!trip) {
+                var newTrip = new TripSchema({ userId, launchId });
+                trip = await newTrip.save();
+            }
+            return [trip];
+        },
+        findAll: async({ userId }) => {
+            var res = await TripSchema.find({ userId }).exec();
+            return res;
+        },
+        destroy: async ({ userId, launchId }) => {
+            var res = await TripSchema.remove({ userId, launchId }).exec();
+            return res.deletedCount > 0;
+        }
+    }
+
+    return { User, Trip }
+}
