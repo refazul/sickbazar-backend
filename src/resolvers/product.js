519a1b89e88aa8308a93f6acac5e6a0e82e54222
refazul <refazul.refat@yahoo.com>
2022-02-02 16:54:11 +0600

    addStock remove to match schema

--- src/resolvers/product.js
+++ src/resolvers/product.js
@@ -19,34 +19,6 @@ module.exports = {
             const response = await dataSources.productAPI.deleteEntity(entityID);
             return entityID;
         },
-        addStock: async (_, { entityID, selector, stock }, { dataSources }) => {
-            const product = await dataSources.productAPI.readEntity(entityID);
-            product.price.push({ selector, stock })
-            const response = await product.save();
-            return {
-                success: response ? "yes" : "no",
-                object: response
-            };
-        },
-        reduceStock: async (_, { entityID, selector, reduceObj }, { dataSources }) => {
-            const product = await dataSources.productAPI.readEntity(entityID);
-
-            const new_stock = product.price.map((p) => {
-                if (object_match(p.selector, selector)) {
-                    const quantity_match = p.stock.quantity > 0 && p.stock.quantity >= reduceObj.quantity;
-                    const vendor_match = p.stock.vendorID == reduceObj.vendorID
-                    if (quantity_match && vendor_match) {
-                        p.stock.quantity -= reduceObj.quantity;
-                    }
-                }
-                return p;
-            });
-            const response = await dataSources.productAPI.updateEntity(entityID, { price: new_stock } );
-            return {
-                success: response ? "yes" : "no",
-                object: response
-            };
-        },
     },
     ProductQueries: {
         readProduct: async (_, { entityID }, { dataSources }) => {
